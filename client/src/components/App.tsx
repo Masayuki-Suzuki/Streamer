5e41f6732c113dd17f81e3df79ca8741834fa40b
Masayuki-Suzuki <m.suzuki.fp@gmail.com>
2020-05-25 10:45:40 -0700

    fix: Fixed package installation mistake. 🐛

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const PageOne = () => {
+    return <div>PageOne</div>
+}
+
+const PageTwo = () => {
+    return <div>PageTwo</div>
+}
 
 function App() {
-    return <div className="App">App!</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/page2" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
